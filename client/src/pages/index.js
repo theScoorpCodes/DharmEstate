fdf634858081121f6e6397545a36d85bd778f80e
Dharm Thakkar <dharm.codes@gmail.com>
2025-06-26 14:22:25 +0530

    Add initial page components: About, Home, Profile, SignIn, and SignUp

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import Home from "./Home";
+import Profile from "./Profile";
+import Signin from "./Signin";
+import SignUp from "./SignUp";
+
+export { About, Home, Profile, Signin, SignUp };
