fb68aeedd674b2008fbfcbb16e4107cb3268b5b4
Dharm Thakkar <dharm.codes@gmail.com>
2025-06-26 16:16:01 +0530

    chore: add mongoose and dotenv dependencies; update MongoDB connection in index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+await mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
+});
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-}  );
+});
