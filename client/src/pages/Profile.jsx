07cd6128b986ccce48bedbe18b5a0579247e3d9b
Dharm Thakkar <dharm.codes@gmail.com>
2025-06-27 16:12:34 +0530

    feat: implement user profile display with avatar and form inputs

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useSelector } from "react-redux";
+
 
 function Profile() {
-  return <div>Profile</div>;
+  const user = useSelector((state) => state.user);
+  return <div className="p-3 max-w-lg mx-auto">
+    <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
+    <form className="flex flex-col gap-4">
+      <img src={user.currentUser.avatar} alt="avatar" className="rounded-full h-24 w-24 object-cover hover:cursor-pointer self-center mt-2" />
+      <input type="text" id="username" placeholder="username" className="border p-3 rounded-lg" />
+      <input type="email" id="email" placeholder="email" className="border p-3 rounded-lg" />
+      <input type="password" id="password" placeholder="password" className="border p-3 rounded-lg" />
+      <button className="bg-slate-700 text-white p-3 rounded-lg mt-4 uppercase hover:opacity-95 disabled:opacity-80">Update</button>
+    </form>
+
+    <div className="flex justify-between items-center mt-4">
+      <span className="text-red-700 cursor-pointer">Delete account</span>
+      <span className="text-red-700 cursor-pointer">sign out</span>
+    </div>
+  </div>;
 }
 
 export default Profile;
