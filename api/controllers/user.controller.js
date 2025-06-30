5694665987f81bec383154efd71784cfc4c14658
Dharm Thakkar <dharm.codes@gmail.com>
2025-06-30 20:24:06 +0530

    feat: add getUserListings functionality to retrieve user-specific listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/Error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 const userTest = (req, res) => {
   res.send("User contoller route is working");
@@ -42,4 +43,15 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
-export { userTest, updateUser, deleteUser };
+const getUserListings = async (req, res, next) => {
+  if(req.user.id !== req.params.id) return next(errorHandler(403, "You are not allowed to get this user's listings"));
+
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { userTest, updateUser, deleteUser, getUserListings};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { userTest, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { userTest, updateUser, deleteUser, getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/VerifyUser.js";
 const router = express.Router();
 
 router.get("/test", userTest);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser)
+router.get("/listings/:id", verifyToken, getUserListings)
 
 export default router;
