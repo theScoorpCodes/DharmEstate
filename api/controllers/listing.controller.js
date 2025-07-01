65d7045c581d0cfaea057a2c32da6db9bba90ebd
Dharm Thakkar <dharm.codes@gmail.com>
2025-07-01 14:47:42 +0530

    feat: add getListings functionality and update routes for listing retrieval

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -3,49 +3,59 @@ import { errorHandler } from "../utils/Error.js";
 import cloudinary from "../utils/Cloudinary.js";
 
 const createListing = async (req, res, next) => {
-    try {
-      const listing = await Listing.create(req.body);
-      return res.status(200).json(listing);
-    } catch (error) {
-      next(error);
-    }
-}
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteListing = async (req, res, next) => {
-  try{
+  try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing) return next(errorHandler(404, "Listing not found"));
-    if(listing.userRef !== req.user.id) return next(errorHandler(403, "You are not allowed to delete this listing"));
+    if (!listing) return next(errorHandler(404, "Listing not found"));
+    if (listing.userRef !== req.user.id)
+      return next(
+        errorHandler(403, "You are not allowed to delete this listing")
+      );
 
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing deleted successfully");
   } catch (error) {
     next(error);
   }
-}
+};
 
 const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
-  if(!listing) return next(errorHandler(404, "Listing not found"));
-  if(listing.userRef !== req.user.id) return next(errorHandler(403, "You are not allowed to update this listing"));
+  if (!listing) return next(errorHandler(404, "Listing not found"));
+  if (listing.userRef !== req.user.id)
+    return next(
+      errorHandler(403, "You are not allowed to update this listing")
+    );
 
   try {
-    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
-}
+};
 
 const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing) return next(errorHandler(404, "Listing not found"));
+    if (!listing) return next(errorHandler(404, "Listing not found"));
     res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
-}
+};
 
 const deleteImage = async (req, res, next) => {
   const { publicId } = req.body;
@@ -55,6 +65,55 @@ const deleteImage = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
-export { createListing, deleteListing, updateListing, getListing, deleteImage }
+const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === "false" || offer === undefined)
+      offer = { $in: [false, true] };
+
+    let furnished = req.query.furnished;
+    if (furnished === "false" || furnished === undefined)
+      furnished = { $in: [false, true] };
+
+    let parking = req.query.parking;
+    if (parking === "false" || parking === undefined)
+      parking = { $in: [false, true] };
+
+    let type = req.query.type;
+    if (type === undefined || type === "all")
+      type = { $in: ["rent", "sell"] };
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export {
+  createListing,
+  deleteListing,
+  updateListing,
+  getListing,
+  deleteImage,
+  getListings,
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,4 +1,4 @@
-import { createListing, deleteListing, updateListing, getListing, deleteImage } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing, getListing, deleteImage, getListings } from "../controllers/listing.controller.js";
 import express from "express";
 import { verifyToken } from "../utils/VerifyUser.js";
 
@@ -9,5 +9,6 @@ router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing)
 router.delete("/delete-image", verifyToken, deleteImage);
+router.get("/get", getListings);
 
 export default router;
